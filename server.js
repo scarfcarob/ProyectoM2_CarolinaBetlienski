7ee3080a5c4031721baf15b10f2bf2e4dae494c5
Carolina Betlienski <scarfcarob@gmail.com>
2026-06-13 20:02:15 -0300

    feat(m2): crear endpoint/health para verificar el estado y uptime del servidor

--- server.js
+++ server.js
@@ -5,17 +5,25 @@ const app = express();
 const PORT = 3000;
 
 // Middleware para parsear JSON
-app.use(express.json());
+//app.use(express.json());
 
-
-
-
-
-
+// Endpoint de salud
+app.get("/health", (req, res) => {
+  res.status(200).json({ 
+    status: "ok",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime()
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+
+
+
 
 
 
