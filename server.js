e23b8c0d1c744998a1f20fb1ccc4972b54feeea1
Carolina Betlienski <scarfcarob@gmail.com>
2026-06-14 00:21:06 -0300

    feat(m2): configurar pool de conexion nativa a pgAdmin y verificar conexion exitosa

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 
 import express from 'express';
+import pool from './src/config/db.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware para parsear JSON
 app.use(express.json());
---
+++ src/config/db.js
@@ -0,0 +1,27 @@
+
+// Cargamos el archivo .env para acceder a las variables de entorno
+process.loadEnvFile();
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+
+})
+
+// TEST DE CONEXION RAPIDO A LA BD
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+    } else {
+        console.log('Conexión exitosa a la base de datos. Hora actual:', res.rows[0].now);
+    }
+});
+
+
+export default pool;
