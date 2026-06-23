3d7b1dabce2364904436ab51938afdcf33504eaf
Carolina Betlienski <scarfcarob@gmail.com>
2026-06-23 02:32:58 -0300

    fix(m2): corregir importacion de controladores nombrados en postsRouter

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -9,7 +9,7 @@ import * as postsController from "../controllers/postsControllers.js";
 const router = Router();
 
 router.get("/", postsController.getAll);
-router.get("/author/:authorId", postsController.getByAuthor); 
+router.get("/author/:authorId", postsController.getByAuthorId); 
 router.get("/:id", postsController.getById);
 router.post("/", postsController.create);
 router.put("/:id", postsController.update);
