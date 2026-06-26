35e83deb0cccb6e9e74775ed8811f04c1589a8fa
Carolina Betlienski <scarfcarob@gmail.com>
2026-06-25 21:55:52 -0300

    fix(m2): refactorizar db.js para usar variables de entorno locales y de produccion

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ const pool = new Pool({
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
 });
 
 // TEST DE CONEXION RAPIDO A LA BD
